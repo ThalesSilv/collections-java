b7422475883a02375d0466c13bad9a4bb774c8ac
Thales Gonçalves <thales.g.silva@hotmail.com>
2023-10-08 18:01:21 -0300

    commit teste2

--- collections-java/src/teste.java
+++ collections-java/src/teste.java
@@ -2,5 +2,6 @@
 public class teste {
 	public static void main(String[] args) {
 		System.out.println("adiciona linha para commit teste");
+		System.out.println("outra linha so para confirmar");
 	}
 }
